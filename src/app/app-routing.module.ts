9d0b1fab2abb4f913c82303ae7f34bff479e2b5e
Lulu <lulumutuli24@gmail.com>
2018-06-04 08:19:57 +0300

    resolved merge conflicts before deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HairComponent } from './hair/hair.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-<<<<<<< HEAD
-import { HairDetailComponent } from './hair-detail/hair-detail.component';
-=======
 import { HairDetailComponent }  from './hair-detail/hair-detail.component';
 
->>>>>>> 424d63719f976c5818997ec0f6b875664e507558
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HairComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HairDetailComponent },
-<<<<<<< HEAD
-=======
 
->>>>>>> 424d63719f976c5818997ec0f6b875664e507558
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,24 +8,23 @@ import { HairService } from '../hair.service';
   styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
-<<<<<<< HEAD
+
   heroes: Hero[]=[];
-=======
+
   heroes: Hero[] = [];
->>>>>>> 424d63719f976c5818997ec0f6b875664e507558
+
 
   constructor(private hairService: HairService) { }
 
   ngOnInit() {
     this.getHeroes();
-<<<<<<< HEAD
+
   }
 
   getHeroes(): void{
     this.hairService.getHeroes()
         .subscribe(heroes => this.heroes = heroes.slice(1, 5));
-=======
->>>>>>> 424d63719f976c5818997ec0f6b875664e507558
+
   }
 
   getHeroes(): void {
